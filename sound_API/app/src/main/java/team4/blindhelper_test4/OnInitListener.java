0ee70801d5cba2d1cacb1be5a53585e8d785210f
KIMKYUBO <rbqhrla@naver.com>
2017-06-13 16:47:13 +0900

    add api

---
+++ sound_API/app/src/main/java/team4/blindhelper_test4/OnInitListener.java
@@ -0,0 +1,9 @@
+package team4.blindhelper_test4;
+
+/**
+ * Created by USER on 2017-06-12.
+ */
+
+public interface OnInitListener {
+    public void onInit(int status);
+}
---
+++ sound_API/app/src/main/java/team4/blindhelper_test4/SpeechToText.java
@@ -0,0 +1,70 @@
+package team4.blindhelper_test4;
+
+/**
+ * Created by USER on 2017-06-12.
+ */
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Locale;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SpeechToText extends Activity
+{
+    private TextToSpeech myTTS;
+    private TextView txtSpeechInput;
+    //private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private String DEST;
+
+
+
+    protected void promptSpeechInput() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                    DEST = result.get(0);
+
+                }
+                break;
+            }
+        }
+    }
+
+
+}
