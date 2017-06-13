b0dc7ee1bf6e33f77dd6e48f81a02f16b5ada49b
Ysunny <ysunny32@gmail.com>
2017-06-14 06:56:01 +0900

    f

--- app/src/main/java/einys/test2/MapsActivity.java
+++ app/src/main/java/einys/test2/MapsActivity.java
@@ -1,15 +1,22 @@
 package einys.test2;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Handler;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,9 +25,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.nearby.connection.Payload;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.skp.Tmap.TMapTapi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -32,15 +41,37 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Locale;
 
 import javax.net.ssl.HttpsURLConnection;
+
+import static android.speech.tts.TextToSpeech.QUEUE_ADD;
+import static android.speech.tts.TextToSpeech.QUEUE_FLUSH;
+
+/**
+ * Created by Google Corp. and Kim Yangsun on 2017-06-11.
+ */
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient = null;
     private static final int MY_LOCATION_REQUEST_CODE = 1;
-    private HttpURLConnection conn = null;
+    private CurLatLng cLatLng = null;
+    private MyLatLng myDest = null;
+    private LatLng curLatLng = null;
+    private LatLng destLatLng = null;
+    private ArrayList<wayPoint> wayPoints = null;
+    private String curLocAddr;
+    private TcpClient tc;
+
+    private TextToSpeech myTTS;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private String DEST; // STT로 변환한 TEXT값
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,28 +83,85 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         TMapTapi tmaptapi = new TMapTapi(this);
-        tmaptapi.setSKPMapAuthentication ("6c5dc4df-49da-32f6-ad3b-4bf375e36ce9");
+        tmaptapi.setSKPMapAuthentication("6c5dc4df-49da-32f6-ad3b-4bf375e36ce9");
+
+
+
+
+        tc = new TcpClient("192.168.42.139", 8001);
+
+
+        cLatLng = new CurLatLng(0.0, 0.0, new CurLatLng.ChangeListener() {
+            @Override
+            public void onChange() {
+
+            }
+
+            @Override
+            public void onInit() {
+
+            }
+        });
+
+        myDest = new MyLatLng(0.0, 0.0, new MyLatLng.ChangeListener() {
+            @Override
+            public void onSetName() {
+                navigate();
+            }
+
+            @Override
+            public void onChange() {
+
+            }
+
+            @Override
+            public void onInit() {
+
+            }
+        });
+
+        myTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                //myTTS.speak("시작", QUEUE_ADD, null, "init")
+
+                promptSpeechInput();
+
+            }
+        });
+
+
+
+        //route(curLatLng);
+
+
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
 
             mMap.setMyLocationEnabled(true);
+
+        } else {
+            // Request permission.
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_REQUEST_CODE);
+        }
+
+        mark(mMap);
+    }
+
+
+    private void navigate(){
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
 
             mFusedLocationClient = new FusedLocationProviderClient(this);
             mFusedLocationClient.getLastLocation()
@@ -82,26 +170,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         public void onSuccess(Location location) {
                             // Got last known location. In some rare situations this can be null.
                             if (location != null) {
+
+                                curLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                                //현재위치에 마커
                                 MarkerOptions markerOptions = new MarkerOptions();
-                                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-
-                                markerOptions.position(latLng);
+                                markerOptions.position(curLatLng);
                                 mMap.clear();
-                                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                                mMap.animateCamera(CameraUpdateFactory.newLatLng(curLatLng));
                                 mMap.addMarker(markerOptions);
-                                Log.i("Location", latLng.latitude + " " + latLng.longitude);
+                                Double lat = curLatLng.latitude;
+                                Double lng = curLatLng.longitude;
+                                //~ 현재위치에 마커
+
+                                TmapHttpReq thr = new TmapHttpReq("http://apis.skplanetx.com/tmap/geo/reversegeocoding?" +
+                                        "lon="+Double.toString(curLatLng.longitude)+"&callback=&coordType=WGS84GEO&addressType=&lat="+Double.toString(curLatLng.latitude)+"&version=1");
+                                thr.threadGET.start();
+                                try {
+                                    thr.threadGET.join();
+                                    String s = thr.getResponse();
+                                    JSONObject jo = new JSONObject(s);
+                                    curLocAddr = jo.getJSONObject("addressInfo").getString("fullAddress");
+                                    ;
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
 
 
-                                // ...
-                                Log.i("rqtest", requestHttpGet("http://apis.skplanetx.com/tmap/routes?version=1&callback=%22%22&startX=14&startY=1&endX=1&endY=1"));
+                                myTTS.speak("현재 위치"+ curLocAddr + "좌표는 경도"+curLatLng.latitude+ " 위도"+curLatLng.longitude+ "입니다.", QUEUE_ADD, null, "curLoc");
+                                Log.i("Location", curLatLng.latitude + " " + curLatLng.longitude);
+
+                                cLatLng.setLatLng(curLatLng.latitude, curLatLng.longitude);
+                                route(curLatLng);
+
+
                             }
                         }
                     });
-
-            TMapTapi tmaptapi = new TMapTapi(this);
-            tmaptapi.setSKPMapAuthentication ("6c5dc4df-49da-32f6-ad3b-4bf375e36ce9");
-
-
 
 
         } else {
@@ -110,9 +217,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     MY_LOCATION_REQUEST_CODE);
         }
 
-
-        mark(mMap);
     }
+
+    public interface LocationResponse {
+        void onResponseReceived(LatLng curLoc);
+    }
+
+
+    private void route(LatLng curLatLng) {
+
+        myTTS.speak("목적지는" + DEST, QUEUE_ADD, null, "dest");
+        myTTS.speak("경로를 탐색합니다.", QUEUE_ADD, null, "dest");
+
+        String endName = "삼가동 진우아파트";
+        GoogleGeoRequest ggr = new GoogleGeoRequest(endName);
+        ggr.addr2co.start();
+
+        try {
+            ggr.addr2co.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        destLatLng = ggr.getResultLatlng();
+
+        /*
+                       404에러 이런거 핸들링하기 @
+         */
+
+        myTTS.speak("경로를 안내합니다.", QUEUE_ADD, null, "dest");
+
+
+        TmapHttpReq thr = new TmapHttpReq("http://apis.skplanetx.com/tmap/routes?version=1&callback=%22%22&" +
+                "startX="+Double.toString(curLatLng.longitude) +
+                "&startY="+Double.toString(curLatLng.latitude) +
+                "&endX=" +Double.toString(destLatLng.longitude)+
+                "&endY="+Double.toString(destLatLng.latitude)+
+                "&startName="+"현재위치"+
+                "&endName="+"목적지"+
+                "&reqCoordType="+"WGS84GEO"+
+                "&resCoordType="+"WGS84GEO");
+        thr.threadGET.start();
+        try {
+            thr.threadGET.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        wayPoints = thr.getWayPoints();
+        speakWays(wayPoints);
+
+        Log.i("Location22", curLatLng.latitude + " " + curLatLng.longitude);
+
+    }
+
+    private void speakWays(ArrayList<wayPoint> wayPoints){
+        for(wayPoint wp : wayPoints){
+            String s = wp.nextDir;
+            myTTS.speak(s, QUEUE_ADD, null, "dir");
+        }
+    }
+
 
     private void mark(final GoogleMap map) {
 
@@ -127,10 +290,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 markerOptions.title(latLng.toString());
 
                 map.clear();
-
                 map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-
                 map.addMarker(markerOptions);
+                tc.sendSTOP();
 
             }
 
@@ -141,62 +303,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    public String requestHttpGet(final String urlStr){
-
-        final StringBuilder sb = new StringBuilder();
-
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Say command...");
         try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Your device not support speech input",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
 
-            URL url = new URL(urlStr); //요청 URL을 입력
-            conn = (HttpURLConnection) url.openConnection();
+    //@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-            new Thread(){
-                public void run(){
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
 
-                    try {
-                        conn.setRequestMethod("POST"); //요청 방식을 설정 (default : GET)
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    DEST = result.get(0);
+                    myDest.setName(DEST);
 
-                        conn.setDoInput(true); //input을 사용하도록 설정 (default : true)
-                        conn.setDoOutput(true); //output을 사용하도록 설정 (default : false)
-
-                        conn.setConnectTimeout(60); //타임아웃 시간 설정 (default : 무한대기)
-
-                        OutputStream os = conn.getOutputStream(); // 서버로 보내기 위한 출력 스트림
-                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8")); // UTF-8로 전송
-                        bw.write(""); // 매개변수 전송
-                        bw.flush();
-                        bw.close();
-                        os.close();
-
-                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")); //캐릭터셋 설정
-
-                        String line = null;
-                        while ((line = br.readLine()) != null) {
-                            if (sb.length() > 0) {
-                                sb.append("\n");
-                            }
-                            sb.append(line);
-                        }
-
-                        System.out.println("response:" + sb.toString());
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    //myTTS.speak(DEST+"가 맞습니까", TextToSpeech.QUEUE_ADD, null); // 주소 되물어보기
                 }
-
-            }.start();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if(conn != null) {
-                conn.disconnect();
+                break;
             }
         }
 
-        return sb.toString();
-
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myTTS.shutdown();
+    }
+
+}
+
 /*
 //Google Play 서비스 접근승인 요청
 public GoogleApiClient.Builder setGoogleServiceBuilder(){
@@ -230,7 +381,7 @@ return mGoogleApiClientBuilder;
 
 */
 
-}
+
 
 /*
 class CallbackConnectedGoogleService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
